ef094179fe6ec0aa266b79f57cfea4a823bc8888
Maris Lakss <maris.lakss@gmail.com>
2023-04-10 12:30:28 +0300

    Initial Commit

---
+++ TwitterDemo/TwitterDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterDemoApp.swift
+//  TwitterDemo
+//
+//  Created by Māris Lakšs on 10/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TwitterDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
